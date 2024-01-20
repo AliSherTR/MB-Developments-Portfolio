be55ffa2d5a936c02bbbcb9bbc376e40738c4bcd
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-01-20 22:32:07 +0500

    Responsive navbar

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,6 @@ import Slider from "react-slick";
 export default function Hero() {
     const settings = {
         dots: false,
-        fade: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
@@ -15,57 +14,45 @@ export default function Hero() {
         autoplay: true,
     };
     return (
-        <Slider {...settings} className="">
-            <div className="h-[90dvh] relative overflow-hidden">
-                <img
-                    src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnVzaW5lc3MlMjBtZWV0aW5nfGVufDB8fDB8fHww"
-                    alt=""
-                    className="absolute inset-0 -z-10 w-screen h-100 block "
-                />
+        <Slider {...settings}>
+            <div className="w-full h-[90vh] bg-cover bg-no-repeat bg-center">
                 <div className="absolute inset-0 bg-black opacity-50"></div>
-                <div className="text-2xl leading-10 text-white font-roboto relative z-20 ms-8 flex flex-col items-start justify-center h-full gap-8">
-                    <p className=" w-[30%]">
+                <div className="text-2xl leading-10 text-white font-roboto relative z-20 flex  items-center justify-center h-full gap-8">
+                    <p className=" text-center">
                         Established with a passion for innovation and a
                         commitment to excellence. MB Developments stands at the
                         forefront of the ever-evolving technology landscape
+                        <button
+                            href="#"
+                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 block m-auto"
+                        >
+                            Get started
+                        </button>
                     </p>
-                    <button>Get Started</button>
                 </div>
             </div>
 
-            <div className="h-[90dvh] relative overflow-hidden">
+            {/* <div className="h-[90dvh] relative overflow-hidden">
                 <img
                     src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
                     alt=""
-                    className="absolute inset-0 -z-10 w-screen h-100 block "
+                    className="absolute inset-0 -z-10 w-[100vw] h-[100vh]  block "
                 />
                 <div className="absolute inset-0 bg-black opacity-50"></div>
-                <div className="text-2xl leading-10 text-white font-roboto relative z-20 ms-8 flex flex-col items-start justify-center h-full gap-8">
-                    <p className=" w-[30%]">
+                <div className="text-2xl leading-10 text-white font-roboto relative z-20 ms-8 flex flex-col items-center justify-center h-full gap-8">
+                    <p className="md:w-[30%] ">
                         Established with a passion for innovation and a
                         commitment to excellence. MB Developments stands at the
                         forefront of the ever-evolving technology landscape
                     </p>
-                    <button>Get Started</button>
+                    <a
+                        href="#"
+                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Get started
+                    </a>
                 </div>
-            </div>
-
-            <div className="h-[90dvh] relative overflow-hidden">
-                <img
-                    src="https://st2.depositphotos.com/2001755/5443/i/450/depositphotos_54431143-stock-photo-beautiful-landscape.jpg"
-                    alt=""
-                    className="absolute inset-0 -z-10 w-screen h-100 block "
-                />
-                <div className="absolute inset-0 bg-black opacity-50"></div>
-                <div className="text-2xl leading-10 text-white font-roboto relative z-20 ms-8 flex flex-col items-start justify-center h-full gap-8">
-                    <p className=" w-[30%]">
-                        Established with a passion for innovation and a
-                        commitment to excellence. MB Developments stands at the
-                        forefront of the ever-evolving technology landscape
-                    </p>
-                    <button>Get Started</button>
-                </div>
-            </div>
+            </div> */}
         </Slider>
     );
 }
