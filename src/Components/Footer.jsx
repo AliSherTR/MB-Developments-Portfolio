e77030483687e5876bf7017b7a9e36f6b3e8f0f8
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-01-30 16:00:41 +0500

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -52,7 +52,7 @@ export default function Footer() {
                 id="footer"
                 className="p-6 max-w-[1200px] m-auto grid md:grid-cols-3 gap-3 text-white"
             >
-                <div>
+                <div className=" p-4">
                     <a
                         href="#"
                         className="block text-lg mb:text-xl lg:text-2xl font-bold font-inter mb-6"
@@ -78,11 +78,11 @@ export default function Footer() {
                         })}
                     </ul>
                 </div>
-                <div>
+                <div className=" p-4">
                     <h3 className=" text-lg font-semibold mb-6 ">
                         Quick Links
                     </h3>
-                    <ul className=" flex flex-col gap-6">
+                    <ul className=" flex flex-col gap-6 ">
                         {footerLinks.map((link, i) => {
                             return (
                                 <li
@@ -96,8 +96,87 @@ export default function Footer() {
                         })}
                     </ul>
                 </div>
-                <div>
+                <div className=" p-4">
                     <h3 className=" text-lg font-semibold mb-6 ">Contact Us</h3>
+                    <div className="mb-4 grid grid-cols-10 gap-y-1 gap-x-2">
+                        <div className="row-start-1 row-span-2 col-start-1 col-end-1 flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="#fff"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                />
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                                />
+                            </svg>
+                        </div>
+                        <div className=" col-start-2 col-span-8 ">
+                            <span>123 NewYork City, 75th street</span>
+                        </div>
+                        <div className=" row-start-2 col-start-2">
+                            <span>Pakistan</span>
+                        </div>
+                    </div>
+
+                    <div className="mb-4 grid grid-cols-10 gap-y-1 gap-x-2">
+                        <div className="row-start-1 row-span-2 col-start-1 col-end-1 flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                                />
+                            </svg>
+                        </div>
+                        <div className=" col-start-2 col-span-8 ">
+                            <span>+92 335588200</span>
+                        </div>
+                        <div className=" row-start-2 col-start-2">
+                            <span>Pakistan</span>
+                        </div>
+                    </div>
+
+                    <div className=" grid grid-cols-10 gap-y-1 gap-x-2">
+                        <div className="row-start-1 row-span-2 col-start-1 col-end-1 flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                                />
+                            </svg>
+                        </div>
+                        <div className=" col-start-2 col-span-8 ">
+                            <span>example@gmail.com</span>
+                        </div>
+                        <div className=" row-start-2 col-start-2">
+                            <span>Pakistan</span>
+                        </div>
+                    </div>
                 </div>
             </section>
         </footer>
