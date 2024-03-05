d280e8b5784bbe90f20d1214f7a6b31465b44802
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-03-05 17:37:24 +0500

    emails and final touches

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,12 +1,40 @@
+import { useRef } from "react";
 import Button from "./Button";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
 
 export default function ContactUs() {
+    const form = useRef();
+
+    const sendEmail = async (e) => {
+        e.preventDefault();
+
+        try {
+            await emailjs.sendForm(
+                "service_6n140gg",
+                "template_xwh5ya6",
+                form.current,
+                {
+                    publicKey: "d0nE3plItEgjMB9KV",
+                }
+            );
+            toast.success("Email Sent Successfully");
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
     return (
         <section className="py-6">
             <h2 className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-[#444]">
                 Contact Us
             </h2>
-            <form action="" className="flex flex-col md:flex-row">
+            <form
+                action=""
+                ref={form}
+                onSubmit={sendEmail}
+                className="flex flex-col md:flex-row"
+            >
                 <div className=" flex-1 self-center flex flex-col pe-6 gap-y-3">
                     <p className=" text-base text-[#444] ">
                         Hey there! 🚀 Ready to take the next step? Drop us a
@@ -21,6 +49,7 @@ export default function ContactUs() {
                             type="text"
                             placeholder="First Name"
                             className="border px-2 py-4 flex-1 rounded-sm"
+                            name="from_name"
                         />
                         <input
                             type="text"
@@ -32,14 +61,16 @@ export default function ContactUs() {
                         type="email"
                         placeholder="example@gmail.com"
                         className="border px-2 py-4 flex-1 rounded-sm"
+                        name="from_name"
                     />
                     <textarea
                         placeholder="Write your queries here"
                         className=" border px-2 py-4 flex-1 rounded-sm"
+                        name="message"
                     />
-                    <div>
+                    <button onClick={sendEmail} className=" me-auto">
                         <Button text={"Submit"} />
-                    </div>
+                    </button>
                 </div>
                 <div className=" flex-1">
                     <img
